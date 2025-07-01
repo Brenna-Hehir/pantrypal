cce9e8d67bf908b718dafc49a3ac273276f1d654
Brenna-Hehir <brennahehir@gmail.com>
2025-06-30 22:26:11 -0400

    Updates

---
+++ precache-manifest.4f02ed59176da3415b4c3de338205fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25d2441e3a8bc13da8c84f5a62b6cd9",
+    "url": "/pantrypal/index.html"
+  },
+  {
+    "revision": "92168236819d9a8194d3",
+    "url": "/pantrypal/static/css/main.a36c154a.chunk.css"
+  },
+  {
+    "revision": "a77cbf291e2e39de1a5d",
+    "url": "/pantrypal/static/js/2.e8f85dca.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/pantrypal/static/js/2.e8f85dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1d9bda9e84755b76df",
+    "url": "/pantrypal/static/js/3.542971a0.chunk.js"
+  },
+  {
+    "revision": "92168236819d9a8194d3",
+    "url": "/pantrypal/static/js/main.542e8ba7.chunk.js"
+  },
+  {
+    "revision": "294eac4504f591770fb9",
+    "url": "/pantrypal/static/js/runtime-main.0f2b330a.js"
+  }
+]);
